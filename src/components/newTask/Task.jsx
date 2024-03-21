7073ff788eb3c1ba18b019c747d44337b5c755f1
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2024-03-21 20:23:47 -0300

    Incluido dentro do arquivo Task.jsx função de alterar cor para verde para as tarefas marcadas como concluidas

--- src/components/newTask/Task.jsx
+++ src/components/newTask/Task.jsx
@@ -21,6 +21,14 @@ const Task = () => {
     }
   };
 
+  // Marca a tarefa como concluída ou não concluída
+  const handleTaskCheck = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, checked: !task.checked } : task
+    );
+    setTasks(updatedTasks);
+  };
+
   // Inicia a edição de uma tarefa
   const handleEditTask = (taskId) => {
     setEditingTaskId(taskId);
@@ -61,66 +69,72 @@ const Task = () => {
   // Retorna a estrutura do componente JSX
   return (
     <section className="task">
-      {/* Cabeçalho da lista de tarefas */}
-      <div className="task__header">
-        <ul>
-          <li>Tarefa</li>
-          <li>Status</li>
-          <li>Opções</li>
-        </ul>
-      </div>
-      {/* Barra horizontal decorativa */}
-      <div className="task__barra-horizontal">
-        <img src="retangulo2.svg" alt="" />
-      </div>
-      {/* Lista de tarefas renderizadas */}
-      <div className="task__list">
-        {tasks.map((task) => (
-          <div key={task.id} className="task__renderResult">
-            <li className="task__renderItem">
-              {editingTaskId === task.id ? (
-                <EditTask
-                  task={task}
-                  onSave={(editedTaskText) => handleSaveEdit(task.id, editedTaskText)}
-                  onCancel={handleCancelEdit}
-                />
-              ) : (
-                <>
-                  {task.task}
-                  {/* Ícones para editar e excluir */}
-                  <div className="task__icons">
-                    <div className="task__checkbox">
-                      <input type="checkbox" name="" id="" />
-                    </div>
-                    <div className="task__extra">
-                      <img
-                        src="edit.svg"
-                        alt="Editar"
-                        onClick={() => handleEditTask(task.id)}
-                      />
-                      <img
-                        src="lixeira.svg"
-                        alt="Excluir"
-                        onClick={() => handleDeleteTask(task.id)}
-                      />
-                    </div>
+    {/* Cabeçalho da lista de tarefas */}
+    <div className="task__header">
+      <ul>
+        <li>Tarefa</li>
+        <li>Status</li>
+        <li>Opções</li>
+      </ul>
+    </div>
+    {/* Barra horizontal decorativa */}
+    <div className="task__barra-horizontal">
+      <img src="retangulo2.svg" alt="" />
+    </div>
+    {/* Lista de tarefas renderizadas */}
+    <div className="task__list">
+      {tasks.map((task) => (
+        <div key={task.id} className={`task__renderResult ${task.checked ? 'task__checked' : ''}`}>
+          <li className="task__renderItem">
+            {editingTaskId === task.id ? (
+              <EditTask
+                task={task}
+                onSave={(editedTaskText) => handleSaveEdit(task.id, editedTaskText)}
+                onCancel={handleCancelEdit}
+              />
+            ) : (
+              <>
+                <span style={{ color: task.checked ? 'green' : 'inherit' }}>{task.task}</span>
+                {/* Ícones para editar e excluir */}
+                <div className="task__icons">
+                  <div className="task__checkbox">
+                    <input
+                      type="checkbox"
+                      name=""
+                      id=""
+                      checked={task.checked}
+                      onChange={() => handleTaskCheck(task.id)}
+                    />
                   </div>
-                </>
-              )}
-            </li>
-          </div>
-        ))}
-        {/* Adição de nova tarefa */}
-        <div className="task__renderList">
-          <input
-            type="text"
-            placeholder="Nova tarefa..."
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-          />
-          <button onClick={handleAddTask}>
-            <img id="cruz" src="" alt="" />
-          </button>
+                  <div className="task__extra">
+                    <img
+                      src="edit.svg"
+                      alt="Editar"
+                      onClick={() => handleEditTask(task.id)}
+                    />
+                    <img
+                      src="lixeira.svg"
+                      alt="Excluir"
+                      onClick={() => handleDeleteTask(task.id)}
+                    />
+                  </div>
+                </div>
+              </>
+            )}
+          </li>
+        </div>
+      ))}
+      {/* Adição de nova tarefa */}
+      <div className="task__renderList">
+        <input
+          type="text"
+          placeholder="Nova tarefa..."
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button className="task__additem" onClick={handleAddTask}>
+          <img id="cruz" src="" alt="" />
+        </button>
         </div>
       </div>
       {/* Modal para editar ou excluir tarefas */}
